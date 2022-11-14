4f434213fa1f2673815fc584c708cb6e24de6f13
bzceval <busraaceval@gmail.com>
2022-11-15 01:45:50 +0300

    add navbar template

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Hello</h1>
     </div>
   );
