1c1b37356b823bdec817437481cca27797f3974c
Captain Price <murathoncu@gmail.com>
2022-11-15 01:56:02 +0300

    add footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Footer />
     </div>
   );
 }
