90fbd5b80ff4fd8dc01e44a3be782c7e57b1e206
bzceval <busraaceval@gmail.com>
2022-11-15 01:48:36 +0300

    change brand name

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = () => {
         <div className="container">
           <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <a href="/" className="brand-name d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-              Drink Coffee
-              
+              Device Coffee
+
             </a>
             <ul className="nav col-12 col-lg-auto mx-auto me-lg-auto mb-2 justify-content-center mb-md-0">
               <li><a href="#" className="nav-link px-3 text-secondary">Home</a></li>
