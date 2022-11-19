83b019b800f4f3b0dec8796fdf7e6e1004974902
bzceval <busraaceval@gmail.com>
2022-11-19 06:24:30 +0300

    done navbar design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,32 +2,29 @@ import React, { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { logout } from "../../auth/firebase";
 import { MainContext } from "../../context/ContextProvider";
+import Logo from '../../helpers/logo.png'
 
 const Navbar = () => {
   const { currentUser } = useContext(MainContext);
 
   return (
-    <header className="p-3 bg-dark">
+    <header className="p-3">
       <div className="container">
         <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-          <NavLink
-            to="/"
-            className="brand-name d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-            Device Coffee
-          </NavLink>
+          <NavLink to="/"> <img src={Logo} alt="" width="60"/> </NavLink>
           <ul className="nav col-12 col-lg-auto mx-auto me-lg-auto mb-2 justify-content-center mb-md-0">
             <li>
-              <NavLink to="/" className="nav-link px-3 text-secondary">
+              <NavLink to="/" className="nav-link px-4">
                 Home
               </NavLink>
             </li>
             <li>
-              <NavLink to="/places-drink" className="nav-link px-3 text-white">
+              <NavLink to="/places-drink" className="nav-link px-4">
                 Places to Drink
               </NavLink>
             </li>
             <li>
-              <NavLink to="/drinks" className="nav-link px-3 text-white">
+              <NavLink to="/drinks" className="nav-link px-4">
                 Drinks
               </NavLink>
             </li>
@@ -37,7 +34,7 @@ const Navbar = () => {
               <div className="d-flex align-items-center">
                 <h4>{currentUser.displayName || "Guest"}</h4>
                 <button
-                  className="btn btn-outline-danger ms-2"
+                  className="btn btn-warning ms-2"
                   onClick={() => logout()}>
                   Logout
                 </button>
@@ -50,7 +47,7 @@ const Navbar = () => {
                   </button>
                 </Link>
                 <Link to="/sign-up">
-                  <button type="button" className="btn btn-outline-warning">
+                  <button type="button" className="btn btn-warning">
                     Sign-up
                   </button>
                 </Link>
