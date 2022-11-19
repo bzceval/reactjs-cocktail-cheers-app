d0b565d884d7e769611a622d9b6dd322ec20e71d
bzceval <busraaceval@gmail.com>
2022-11-19 06:42:44 +0300

    done footer design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,70 +3,23 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <>
-      <div className="bg-dark text-light">
-        <footer>
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-6 col-md-3 item">
-                <h3>Services</h3>
-                <ul className="list-unstyled">
-                  <li>
-                    <Link to="#">Web design</Link>
-                  </li>
-                  <li>
-                    <Link to="#">Development</Link>
-                  </li>
-                  <li>
-                    <Link to="#">Hosting</Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-sm-6 col-md-3 item">
-                <h3>About</h3>
-                <ul className="list-unstyled">
-                  <li>
-                    <Link to="#">Company</Link>
-                  </li>
-                  <li>
-                    <Link to="#">Team</Link>
-                  </li>
-                  <li>
-                    <Link to="#">Careers</Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-md-6 item text">
-                <h3>Company Name</h3>
-                <p>
-                  Praesent sed lobortis mi. Suspendisse vel placerat ligula.
-                  Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam
-                  quis tristique lectus. Aliquam in arcu eget velit pulvinar
-                  dictum vel in justo.
-                </p>
-              </div>
-              <div className="col item social text-center fs-3">
-                <Link to="#">
-                  <BsFacebook />
-                </Link>
-                <Link to="#">
-                  <BsTwitter />
-                </Link>
-                <Link to="#">
-                  <BsSnapchat />
-                </Link>
-                <Link to="#">
-                  <BsInstagram />
-                </Link>
-              </div>
-            </div>
-            <p className="copyright display-6 text-center">
-              © TEST {new Date().getFullYear()}
-            </p>
+    <footer className="py-4">
+      <div className="container">
+        <div className="row">
+        <div className="text-center">
+            <h3>Project Description</h3>
+            <p className="small"> Praesent sed lobortis mi. Suspendisse vel placerat ligula. </p>
+            <div className="col fs-5">
+            <Link to="#"> <BsFacebook /></Link>
+            <Link to="#"> <BsTwitter /> </Link> 
+            <Link to="#"> <BsInstagram /> </Link>
           </div>
-        </footer>
+          </div>
+        </div>
+        <p className="mt-3 text-center text-muted">
+          © Device Coffee {new Date().getFullYear()} </p>
       </div>
-    </>
+    </footer>
   );
 };
 
