4b63fb8ebdcc6abc1178f270b73fa03981e0a115
Captain Price <murathoncu@gmail.com>
2022-11-19 03:04:39 +0300

    add card hover effect

--- src/components/SelectDrinks/SelectDrink.jsx
+++ src/components/SelectDrinks/SelectDrink.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 const Drink = ({ selectDrink }) => {
   const [queryDrink, setQueryDrink] = useState([]);
-   const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const SELECT_QUERY = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${selectDrink}`;
 
@@ -19,24 +19,28 @@ const Drink = ({ selectDrink }) => {
 
   return (
     <div className="container">
-     <div className="row ">
-    {queryDrink?.map((item) => {
-    const {idDrink, strDrinkThumb, strDrink} = drink
-     return (
-     <div className="col-md-4 flip-card d-flex justify-content-center">
+      <div className="row ">
+        {queryDrink?.map((item) => {
+          const { idDrink, strDrinkThumb, strDrink } = item;
+          return (
+            <div className="col-md-4 flip-card mb-4  d-flex justify-content-center ">
               <div className="flip-card-inner">
                 <div className="flip-card-front">
-                 <div key={item?.idDrink} className="card shadow-sm">
-                 <img src={strDrinkThumb} className="img-fluid" alt="" />
-                  <div className="card-body">
-                  <p>{idDrink}</p>
-                  <h6 className="text-center text-warning">{strDrink}</h6>
-                  <button className="btn btn-warning my-3" onClick={ () => navigate(`/drinks/${idDrink}`, {state: drink}) }>Detail</button>
+                  <div key={idDrink} className="card shadow-sm">
+                    <img src={strDrinkThumb} className="img-fluid" alt="" />
+                    <div className="card-body">
+                      <p>{idDrink}</p>
+                      <h6 className="text-center text-warning">{strDrink}</h6>
+                    </div>
                   </div>
                 </div>
-                <div class="flip-card-back d-flex justify-content-center align-items-center">
-                  <button className="btn btn-outline-primary">
-                    Go Details
+                <div className="flip-card-back d-flex justify-content-center align-items-center">
+                  <button
+                    className="btn btn-danger my-3  px-4 py-2"
+                    onClick={() =>
+                      navigate(`/drinks/${idDrink}`, { state: item })
+                    }>
+                    Detail
                   </button>
                 </div>
               </div>
