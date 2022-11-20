7c54a08716a81bbc188f96b8b4966ff6239026e5
Captain Price <murathoncu@gmail.com>
2022-11-20 23:00:46 +0300

    added background img for login and register pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { BsGoogle } from "react-icons/bs";
 import { Link, useNavigate } from "react-router-dom";
 import { login, signUpGoogle } from "../auth/firebase";
 import "../index.css";
+import backGroundImage from "../helpers/Social interaction.gif";
 
 const Login = () => {
   const [loginEmail, setLoginEmail] = useState("");
@@ -26,84 +27,89 @@ const Login = () => {
   };
 
   return (
-    <div className="login-container">
-      <div className="container ">
-        <div className="row form-bg">
-          <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
-            <div className="card border-0 shadow rounded-3 my-5">
-              <div className="card-body p-4 p-sm-5">
-                <h5 className="card-title text-center mb-5 fw-light fs-5">
-                  Sign In
-                </h5>
-                <form onSubmit={handleSubmit}>
-                  <div className="form-floating mb-3">
-                    <input
-                      type="email"
-                      className="form-control"
-                      id="floatingInput"
-                      placeholder="name@example.com"
-                      onChange={(e) => setLoginEmail(e.target.value)}
-                    />
-                    <label htmlFor="floatingInput">Email address</label>
-                  </div>
-                  <div className="form-floating mb-3">
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="floatingPassword"
-                      placeholder="Password"
-                      onChange={(e) => setLoginPassword(e.target.value)}
-                    />
-                    <label htmlFor="floatingPassword">Password</label>
-                  </div>
-                  <div className="form-check mb-3">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      defaultValue=""
-                      id="rememberPasswordCheck"
-                    />
-                    <label
-                      className="form-check-label"
-                      htmlFor="rememberPasswordCheck">
-                      Remember password
-                    </label>
-                  </div>
-                  <div className="d-grid">
-                    {loading ? (
-                      <img
-                        src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/attachments/delivery/asset/c49c83ef51b0c4230f8f39560b8250a3-1596267998/navy_for-light_bg/make-animated-logo-loader-for-your-website.gif"
-                        alt="spinner"
-                        width="150px"
-                        height="100px"
-                        className="m-auto"
+    <div className="container">
+      <div className="d-flex justify-content-between align-items-center ">
+        <div className="d-none d-lg-block">
+          <img src={backGroundImage} alt="" />
+        </div>
+        <div className="w-100">
+          <div className="row form-bg">
+            <div className="col-sm-9 mx-auto">
+              <div className="card border-0 shadow rounded-3 my-5">
+                <div className="card-body p-4 p-sm-5">
+                  <h5 className="card-title text-center mb-5 fw-light fs-5">
+                    Sign In
+                  </h5>
+                  <form onSubmit={handleSubmit}>
+                    <div className="form-floating mb-3">
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="floatingInput"
+                        placeholder="name@example.com"
+                        onChange={(e) => setLoginEmail(e.target.value)}
                       />
-                    ) : (
-                      <div className="d-grid">
-                        <button
-                          className="btn btn-outline-primary btn-login text-uppercase fw-bold"
-                          type="submit"
-                          disabled={loading}>
-                          Sign In
-                        </button>
-                        <p className="text-center mt-3">
-                          Do you have an account ?
-                          <Link to="/sign-up">Sign Up</Link>
-                        </p>
-                      </div>
-                    )}
-                  </div>
-                  <hr className="my-4" />
-                  <div className="d-grid mb-2">
-                    <button
-                      className="btn btn-outline-danger btn-login text-uppercase fw-bold"
-                      type="submit"
-                      onClick={signGoogle}>
-                      <BsGoogle className="fs-6 me-3 mb-1" />
-                      Sign in with Google
-                    </button>
-                  </div>
-                </form>
+                      <label htmlFor="floatingInput">Email address</label>
+                    </div>
+                    <div className="form-floating mb-3">
+                      <input
+                        type="password"
+                        className="form-control"
+                        id="floatingPassword"
+                        placeholder="Password"
+                        onChange={(e) => setLoginPassword(e.target.value)}
+                      />
+                      <label htmlFor="floatingPassword">Password</label>
+                    </div>
+                    <div className="form-check mb-3">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        defaultValue=""
+                        id="rememberPasswordCheck"
+                      />
+                      <label
+                        className="form-check-label"
+                        htmlFor="rememberPasswordCheck">
+                        Remember password
+                      </label>
+                    </div>
+                    <div className="d-grid">
+                      {loading ? (
+                        <img
+                          src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/attachments/delivery/asset/c49c83ef51b0c4230f8f39560b8250a3-1596267998/navy_for-light_bg/make-animated-logo-loader-for-your-website.gif"
+                          alt="spinner"
+                          width="150px"
+                          height="100px"
+                          className="m-auto"
+                        />
+                      ) : (
+                        <div className="d-grid">
+                          <button
+                            className="btn btn-outline-primary btn-login text-uppercase fw-bold"
+                            type="submit"
+                            disabled={loading}>
+                            Sign In
+                          </button>
+                          <p className="text-center mt-3">
+                            Do you have an account ?
+                            <Link to="/sign-up">Sign Up</Link>
+                          </p>
+                        </div>
+                      )}
+                    </div>
+                    <hr className="my-4" />
+                    <div className="d-grid mb-2">
+                      <button
+                        className="btn btn-outline-danger btn-login text-uppercase fw-bold"
+                        type="submit"
+                        onClick={signGoogle}>
+                        <BsGoogle className="fs-6 me-3 mb-1" />
+                        Sign in with Google
+                      </button>
+                    </div>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BsGoogle } from "react-icons/bs";
 import { Link, useNavigate } from "react-router-dom";
 import { register, signUpGoogle } from "../auth/firebase";
+import backGroundImage from "../helpers/Social interaction.gif";
 
 const Register = () => {
   const [registerEmail, setRegisterEmail] = useState("");
@@ -25,86 +26,91 @@ const Register = () => {
   };
 
   return (
-    <div className="login-container">
-      <div className="container ">
-        <div className="row form-bg">
-          <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
-            <div className="card border-0 shadow rounded-3 my-5">
-              <div className="card-body p-4 p-sm-5">
-                <h5 className="card-title text-center mb-5 fw-light fs-5">
-                  Sign Up
-                </h5>
-                <form onSubmit={handleSubmit}>
-                  <div className="form-floating mb-3">
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="floatingInput"
-                      placeholder="Enter full name..."
-                      onChange={(e) => setFullName(e.target.value)}
-                      required
-                    />
-                    <label htmlFor="floatingInput">Full Name</label>
-                  </div>
-
-                  <div className="form-floating mb-3">
-                    <input
-                      type="email"
-                      className="form-control"
-                      id="floatingInput"
-                      placeholder="name@example.com"
-                      onChange={(e) => setRegisterEmail(e.target.value)}
-                      required
-                    />
-                    <label htmlFor="floatingInput">Email address</label>
-                  </div>
-                  <div className="form-floating mb-3">
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="floatingPassword"
-                      placeholder="Password"
-                      onChange={(e) => setRegisterPassword(e.target.value)}
-                      required
-                    />
-                    <label htmlFor="floatingPassword">Password</label>
-                  </div>
-
-                  <div className="d-grid text-center">
-                    {loading ? (
-                      <img
-                        src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/attachments/delivery/asset/c49c83ef51b0c4230f8f39560b8250a3-1596267998/navy_for-light_bg/make-animated-logo-loader-for-your-website.gif"
-                        alt="spinner"
-                        width="150px"
-                        height="100px"
-                        className="m-auto"
+    <div className="container">
+      <div className="d-flex justify-content-between align-items-center ">
+        <div className="d-none d-lg-block">
+          <img src={backGroundImage} alt="" />
+        </div>
+        <div className="w-100">
+          <div className="row form-bg">
+            <div className="col-sm-9 mx-auto">
+              <div className="card border-0 shadow rounded-3 my-5">
+                <div className="card-body p-4 p-sm-5">
+                  <h5 className="card-title text-center mb-5 fw-light fs-5">
+                    Sign Up
+                  </h5>
+                  <form onSubmit={handleSubmit}>
+                    <div className="form-floating mb-3">
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="floatingInput"
+                        placeholder="Enter full name..."
+                        onChange={(e) => setFullName(e.target.value)}
+                        required
                       />
-                    ) : (
-                      <div className="d-grid">
-                        <button
-                          className="btn btn-outline-primary btn-login text-uppercase fw-bold"
-                          type="submit"
-                          disabled={loading}>
-                          Sign Up
-                        </button>
-                        <p className="text-center mt-3">
-                          Do you have an account ?{" "}
-                          <Link to="/login">Sign In</Link>
-                        </p>
-                      </div>
-                    )}
-                  </div>
-                  <hr className="my-4" />
-                  <div className="d-grid mb-2">
-                    <button
-                      className="btn btn-outline-danger btn-login text-uppercase fw-bold"
-                      type="submit"
-                      onClick={registerWithGoogle}>
-                      <BsGoogle className="fs-6 me-3 mb-1" />
-                      Sign Up with Google
-                    </button>
-                  </div>
-                </form>
+                      <label htmlFor="floatingInput">Full Name</label>
+                    </div>
+
+                    <div className="form-floating mb-3">
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="floatingInput"
+                        placeholder="name@example.com"
+                        onChange={(e) => setRegisterEmail(e.target.value)}
+                        required
+                      />
+                      <label htmlFor="floatingInput">Email address</label>
+                    </div>
+                    <div className="form-floating mb-3">
+                      <input
+                        type="password"
+                        className="form-control"
+                        id="floatingPassword"
+                        placeholder="Password"
+                        onChange={(e) => setRegisterPassword(e.target.value)}
+                        required
+                      />
+                      <label htmlFor="floatingPassword">Password</label>
+                    </div>
+
+                    <div className="d-grid text-center">
+                      {loading ? (
+                        <img
+                          src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/attachments/delivery/asset/c49c83ef51b0c4230f8f39560b8250a3-1596267998/navy_for-light_bg/make-animated-logo-loader-for-your-website.gif"
+                          alt="spinner"
+                          width="150px"
+                          height="100px"
+                          className="m-auto"
+                        />
+                      ) : (
+                        <div className="d-grid">
+                          <button
+                            className="btn btn-outline-primary btn-login text-uppercase fw-bold"
+                            type="submit"
+                            disabled={loading}>
+                            Sign Up
+                          </button>
+                          <p className="text-center mt-3">
+                            Do you have an account ?{" "}
+                            <Link to="/login">Sign In</Link>
+                          </p>
+                        </div>
+                      )}
+                    </div>
+                    <hr className="my-4" />
+                    <div className="d-grid mb-2">
+                      <button
+                        className="btn btn-outline-danger btn-login text-uppercase fw-bold"
+                        type="submit"
+                        onClick={registerWithGoogle}>
+                        <BsGoogle className="fs-6 me-3 mb-1" />
+                        Sign Up with Google
+                      </button>
+                    </div>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
