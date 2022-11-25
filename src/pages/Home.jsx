48aa8dc1f0ad46a3c241824774dca3f3f3bb46b0
bzceval <busraaceval@gmail.com>
2022-11-26 02:04:39 +0300

    add home p

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <>
         <div className="hero">
         <div className="context">
-          <h1>Pure Css Animated Background</h1>
+          <h1>Buraya Açıklamalar gelmeli</h1>
         </div>
         <div className="area">
           <ul className="circles">
